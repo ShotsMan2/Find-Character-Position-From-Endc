1e6459e95035690ebc57eea9e18d1040d102e249
Selim Efe <uysalselimefe@gmail.com>
2026-01-14 12:25:07 +0300

    Implement reverse search logic to find character rank from end
    Added 'Sondan_Karakter_Bul' function. The algorithm determines the string length without standard libraries and iterates from the last index to the first. It returns (Length - Index) to represent the position from the end.

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Sondan_Karakter_Bul(char charDizi[], char aranan){
+    int i=0,len=0,uygun=0;
+
+    while(charDizi[i]!='\0'){
+        i++;
+        len++;
+    }
+
+    for(i=len-1;i>=0;i--){
+        if(charDizi[i]==aranan){
+            uygun=1;
+            break;
+        }
+    }
+    if(uygun==1){
+        return len-i;
+    }
+    else{
+        return -1;
+    }
+}
+
+int main()
+{
+    printf("%d",Sondan_Karakter_Bul("Temeli",'a'));
+}
